d5a0583bb10ec07dd1979bdac33c3d21eb8e2031
mugisha-blaine <n.blaine@alustudent.com>
2023-01-30 09:55:30 -0800

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alpha in range(ord('a'), ord('b') + 1):
-    if chr(alpha) != 'q' and chr(alpha) != 'e':
-        print("{}".format(chr(alpha)), end= '')
+for letter in range(97, 123):
+    if (chr(letter)) != 'q' and (chr(letter)) != 'e':
+        print("{}".format(chr(letter)), end= '')
