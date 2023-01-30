5f29650601e8dad21c612e912e3a59d5791910c2
mugisha-blaine <n.blaine@alustudent.com>
2023-01-30 11:17:58 -0800

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord('a') and or d(c) <= ord('z'):
+    if ord(c) >= ord('a') and d(c) <= ord('z'):
         print(True)
     else:
         print(False)
