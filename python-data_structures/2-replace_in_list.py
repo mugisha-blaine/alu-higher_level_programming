297f96e97c78b7e418dfdf873c62e7ba396785ef
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 12:35:33 -0800

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,6 +2,4 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        my_list[3] = 9
-        return my_list
+    return new_list
