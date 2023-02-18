619b3a1d0027814dae8d261fabc4e4983918638c
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 14:26:56 -0800

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    str1 = my_string.lower()
     new = ""
     for a in str1:
-        if (a != "c"):
+        if (a != "c") or (a != "C"):
             new = new + a
     return new
