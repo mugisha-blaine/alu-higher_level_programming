c54b88842b00374610877f6136b74c680ebf6c8f
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 05:46:35 -0800

    task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     length = len(sys.argv)
     if length == 1:
-        print("{} arguments.".format(length -1))
+        print("{} arguments:".format(length - 1))
     if length == 2:
-        print("{} argument".format(length -1))
+        print("{} argument".format(length - 1))
     else:
-        print("{} arguments".format(lenght -1))
+        print("{} arguments".format(length - 1))
     for a in (1, length):
          print("{}: {}".format(a, sys.argv[a]))
