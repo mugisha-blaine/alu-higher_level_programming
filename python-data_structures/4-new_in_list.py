b45847bb85022f566178c9c4abf0fad08119a474
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 13:26:45 -0800

    task4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,5 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
-    new_list = my_list.copy
-    new_list[idx] = element
+    new_list = my_list.copy()
     return new_list
