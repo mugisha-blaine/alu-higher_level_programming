86cda104da5e165e97afc5d357358609c3805a89
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 05:52:33 -0800

    task 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     if length == 1:
         print("{} arguments:".format(length - 1))
     if length == 2:
-        print("{} argument".format(length - 1))
+        print("{} argument:".format(length - 1))
     else:
-        print("{} arguments".format(length - 1))
-    for a in (1, length):
-         print("{}: {}".format(a, sys.argv[a]))
+        print("{} arguments:".format(length - 1))
+    for a in range(1, length):
+        print("{}: {}".format(a, sys.argv[a]))
