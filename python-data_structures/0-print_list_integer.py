511f4139fb7cf6cdc3b76a5dd53549d3df0667c2
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 11:45:00 -0800

    task 1

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__name__":
-    from 0-main import my_list
 def list(my_list):
-    return my_list
-print(list("{:d}".format(my_list))
+    for a in my_list:
+        print(list("{:d}".format(x))
