f3e21c2dc0fe95f2c813c8fdf701d2acf67bbbf5
mugisha-blaine <n.blaine@alustudent.com>
2023-02-18 12:20:07 -0800

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,4 @@
 def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list):
         return none
-    return my_list[idx]
-print("{}".format(element_at(my_list, idx))) 
+    return my_list[idx] 
