7d826390dde2b1c7b6b25188eed6a727d9513628
mugisha-blaine <n.blaine@alustudent.com>
2023-02-19 13:38:47 -0800

    task

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
+    import dis
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
@@ -8,5 +9,4 @@ def magic_calculation(a, b):
         return c
     else:
         return sub(a, b)
-import dis
 dis.dis(magic_calculation)
