1ac18b8e65681849b2e2d2aab1f6f21cbbb7b1f5
mugisha-blaine <n.blaine@alustudent.com>
2023-02-19 09:54:09 -0800

    task 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tot_a = len(tuple_a)
+    tot_b = len(tuple_b)
+    if tot_a == 0:
+        tuple_a = (0, 0)
+    elif tot_a == 1:
+        tuple_a = tuple_a[0], 0)
+    if tot_b < 1:
+        tuple_b = (0, 0)
+    elif tot_b < 2:
+        tuple_b = tuple_b[0], 0)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
