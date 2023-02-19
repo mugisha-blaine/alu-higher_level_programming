ecf4efaf9fee44127c62289fd82e322fe0e993f3
mugisha-blaine <n.blaine@alustudent.com>
2023-02-19 10:24:58 -0800

    task 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    big = my_list[0]
     length = len(my_list)
     if length == 0:
         return None
+    big = my_list[0]
     for x in my_list:
         if x > big:
             big = x
