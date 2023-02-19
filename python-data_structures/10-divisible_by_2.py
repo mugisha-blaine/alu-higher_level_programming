db78a641dbb713dd30f10abda97e80920e3edcff
mugisha-blaine <n.blaine@alustudent.com>
2023-02-19 10:41:27 -0800

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     new = my_list.copy()
-    for i in range(0, len(my_list)):
+    for x in range(0, len(my_list)):
         if x % 2 == 0:
             new[i] = 1
         else:
