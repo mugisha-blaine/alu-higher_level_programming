4b6b686ed1fb231b09b0512a0f476d6123b93745
mugisha-blaine <n.blaine@alustudent.com>
2023-02-25 10:43:55 -0800

    task 10

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     multi = a_dictionary.copy()
-    for a in multi.keys:
+    for a in multi.keys():
         multi[a] = multi * 2
     return multi
