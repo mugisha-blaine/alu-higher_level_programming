24160f5f0f421468b489c27ccf0d6e8186b9cf21
mugisha-blaine <n.blaine@alustudent.com>
2023-02-25 11:02:27 -0800

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     romans['IV'] = 4
     romans['IX'] = 9
     romans['XL'] = 40
