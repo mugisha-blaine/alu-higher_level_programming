3158adf7f6a409f1901b34ddfb2fbbcc0405408f
mugisha-blaine <n.blaine@alustudent.com>
2023-03-11 09:25:58 -0800

    task 44

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,8 +14,9 @@ class Square:
 
     @property
     def size(self):
-        """Get the size of the square."""
+        """Get/set the size of the square."""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -23,6 +24,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-     def area(self):
+
+    def area(self):
         """Return the area of the square."""
-        return (self.__size * self.__size)  
+        return (self.__size * self.__size)
