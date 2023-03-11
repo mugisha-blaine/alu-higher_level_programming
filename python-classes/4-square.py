5db0ed0c0b19881d8acf1c20fc3297023598e011
mugisha-blaine <n.blaine@alustudent.com>
2023-03-11 09:18:58 -0800

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -16,7 +16,6 @@ class Square:
     def size(self):
         """Get the size of the square."""
         return (self.__size)
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
