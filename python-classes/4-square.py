88b958d6b8e97868cd0d58c06ea44c713645b439
mugisha-blaine <n.blaine@alustudent.com>
2023-03-11 09:21:13 -0800

    task 44

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -24,5 +24,5 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
      def area(self):
-        """Return the  area of the square."""
+        """Return the area of the square."""
         return (self.__size * self.__size)  
