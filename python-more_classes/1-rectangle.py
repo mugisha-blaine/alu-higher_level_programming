2589f6a6bf660e294ee7b8d14ad1b05551c2b664
mugisha-blaine <n.blaine@alustudent.com>
2023-03-12 09:30:23 -0700

    task 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Define c class rectangle."""
+"""Define a class rectangle."""
 
 
 class Rectangle:
-    """Present a rectangle"""
+    """Represent a rectangle."""
 
      def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
