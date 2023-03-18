f3b066bfb95327990acb1ef12b00ef258e78868e
mugisha-blaine <n.blaine@alustudent.com>
2023-03-18 09:29:15 -0700

    task 30

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(b):
-    for n in range(10):
-        return b * n
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
