9288f819fa95bc6ff0f32731e2c12a641f3e9908
mugisha-blaine <n.blaine@alustudent.com>
2023-03-19 05:30:06 -0700

    task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' A rectangle class'''
+
+
+Rectangle = __import__('9-rectangle.py').Rectangle
+
+
+class Square(Rectangle):
+    ''' define square'''
+    def __init__(self, size):
+        '''instantiation with size '''
+        self.__size = size
+    def area(self):
+        ''' find an area '''
+        return self.__size * self.__size
