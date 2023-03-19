167dda38cc9ecc0676929d744a209149912bd0f2
mugisha-blaine <n.blaine@alustudent.com>
2023-03-19 06:25:06 -0700

    last

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+square class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square Class """
+    def __init__(self, size):
+        """ instantiation with size """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        ''' implement area '''
+        return self.__size * self.__size
+
+     def __str__(self):
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
