5d6b4a46bc6e62509574e38f43e7d01963e04738
mugisha-blaine <n.blaine@alustudent.com>
2023-03-19 11:32:06 -0700

    task 12

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines a Pascal's Triangle function."""
+
+
+def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        triangle = triangles[-1]
+        triple = [1]
+        for x in range(len(triangle) - 1):
+            triple.append(triangle[i] + triangle[i + 1])
+        triple.append(1)
+        triangles.append(triple)
+    return triangles
