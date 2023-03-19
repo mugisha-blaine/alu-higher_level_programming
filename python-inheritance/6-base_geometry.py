72268de48cd13e07b2446b57889db2374c389d14
mugisha-blaine <n.blaine@alustudent.com>
2023-03-19 04:36:13 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" define an empty class."""
+
+
+class BaseGeometry:
+    """ an empty class"""
+
+    def area(self):
+        """Not done."""
+        raise Exception('area() is not implemented')
