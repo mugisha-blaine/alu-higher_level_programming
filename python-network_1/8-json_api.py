bfbd25a1468e39437678606e436559c88d7fca88
mugisha-blaine <n.blaine@alustudent.com>
2023-04-02 20:56:53 +0000

    task 8

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -10,12 +10,11 @@ if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     response = requests.post(url, data={'q': q})
     try:
-        respo_dict = response.json()
-        id, name = respo_dict.get('id'), respo_dict.get('name')
-        if len(respo_dict) == 0 or not id or not name:
+        res_dict = response.json()
+        id, name = res_dict.get('id'), res_dict.get('name')
+        if len(res_dict) == 0 or not id or not name:
             print("No result")
         else:
-            print("[{}] {}".format(respo_dict.get('id'), \
-respo_dict.get('name')))
+            print("[{}] {}".format(res_dict.get('id'), res_dict.get('name')))
     except:
         print("Not a valid JSON")
