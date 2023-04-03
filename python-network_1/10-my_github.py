f896ec9d11644e64cec0fe35efef596f29ae6b92
mugisha-blaine <n.blaine@alustudent.com>
2023-04-03 10:12:44 +0000

    task 10

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Python script that takes your Github credentials (username and password)
+script that takes your Github credentials (username and password)
 and uses the Github API to display your id
 """
+
+
 import sys
 import requests
 
