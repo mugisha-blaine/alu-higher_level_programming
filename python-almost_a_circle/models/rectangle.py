510670b7ffe3cc1657750802961fa14ec8250ce5
mugisha-blaine <n.blaine@alustudent.com>
2023-04-14 05:23:07 -0700

    okk

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,80 +1,110 @@
 #!/usr/bin/python3
-'''
-    Class Rectangle
-'''
+"""
+    contains Rectangle class that implements Base class
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''
-        Defining the Rectangle class
-        Inherits from:
-            Base
-    '''
-
+    """
+        class Rectangle implements Base class
+        Methods:
+            __init__()
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+            Initializes class instance
+        """
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        super().__init__(id)
 
     @property
     def width(self):
-        '''
-            Returning private attribute
-        '''
+        """
+            getter function for __width
+            Returns: width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("width", value)
+        """
+            setter function for width
+            Args:
+                value (int): value to be set
+        """
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
     def height(self):
-        '''
-            Returning private attribute
-        '''
+        """
+            getter function for height
+            Returns: height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("height", value)
+        """
+            setter function for height
+            Args:
+                value (int): value to be set
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
     def x(self):
-        '''
-            Returning private attribute
-        '''
+        """
+            getter function for x
+            Returns: x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("x", value)
+        """
+            setter function for x
+            Args:
+                value (int): value to be set
+        """
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
     def y(self):
-        '''
-            Returning private attribute
-        '''
+        """
+            getter function for y
+            Returns: y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''
-            Setting private attribute
-        '''
-        self.setter_validation("y", value)
+        """
+            setter function for y
+            Args:
+                value (int): value to be set
+        """
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = value
