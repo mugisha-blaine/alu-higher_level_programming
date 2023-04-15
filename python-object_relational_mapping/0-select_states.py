112ceb9302a40877d7801db0d82b49b294dc7e8f
mugisha-blaine <n.blaine@alustudent.com>
2023-04-15 10:15:25 -0700

    task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 
-import MySQLdb
-from sys import argv
 
 '''
 a script that lists all states
 from the database
 '''
+import MySQLdb
+from sys import argv
+
 if __name__ == "__main__":
     conn = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1],
