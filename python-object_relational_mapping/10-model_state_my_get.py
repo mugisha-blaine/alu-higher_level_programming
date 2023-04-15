1eafc9cefbf0a572a628f018e1e7666b29ec3aa0
mugisha-blaine <n.blaine@alustudent.com>
2023-04-15 14:24:20 -0700

    task 11

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""
-All states via SQLAlchemy
-"""
+'''
+a script that lists all State objects
+'''
+
+
 from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                        argv[2],
+                                                        argv[3]))
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
-    data = session.query(State).order_by(State.id)\
-                               .filter(State.name.like("%a%")).all()
-    for row in data:
-        print("{}: {}".format(row.id, row.name))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+            print(str(state.id))
+    else:
+        print("Not found")
