0bcee438953da166895c6c076b42c1a304a43608
mugisha-blaine <n.blaine@alustudent.com>
2023-06-06 06:04:50 -0700

    task 7

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const coding = [ 'C is fun', 'Python is cool', 'JavaScript is amazing'];
+const coding = [ 'C is fun', 'Python is cool', 'JavaScript is amazing' ];
 for (let i = 0; i < coding.length; i++){
 	console.log(coding[i]);
 }	
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const first = parseInt(process.argv[2]);
 const fun = 'C is fun';
-if (!isNaN(first)){
-	for ( let i = 0; i < first; i++){
-	        console.log(fun);
-	}
-
+if (!isNaN(first)) {
+  for ( let i = 0; i < first; i++){
+    console.log(fun);
+  }
 } else {
 	console.log('Missing number of occurrences');
 }
