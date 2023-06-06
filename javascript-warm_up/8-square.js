c8cba0b7cd6814b71c46468e838eac187388fc75
mugisha-blaine <n.blaine@alustudent.com>
2023-06-06 06:39:44 -0700

    task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const fun = 'X';
 if (!isNaN(square)) {
   for (let i = 0; i < square; i++) {
     let times = '';
-    for (let j = 0; i < square; j++) {
+    for (let j = 0; j < square; j++) {
       times = times + fun;
     }
     console.log(times);
