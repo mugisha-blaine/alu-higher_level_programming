2c68c880438126f072493d84920b8adb709f604a
mugisha-blaine <n.blaine@alustudent.com>
2023-06-06 05:21:01 -0700

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const number = process.argv[2];
 const convert = parseInt(number);
-if (isNaN(number)) {
+if (isNaN(converti)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + convert);
+}
