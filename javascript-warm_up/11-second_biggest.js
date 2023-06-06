6ceab292b4510483fe53a0ae26a28b978a3f1471
mugisha-blaine <n.blaine@alustudent.com>
2023-06-06 07:54:22 -0700

    task 11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const big = process.argv.slice(2).map(Number);
-const biggest = args.sort((a, b) => b - a);
+const biggest = big.sort((a, b) => b - a);
 
 if (big.length <= 1) {
   console.log(0);
