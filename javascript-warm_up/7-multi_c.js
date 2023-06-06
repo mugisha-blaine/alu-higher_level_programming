48b9c2c08b0313bdeeb644e2d911c01c0c38d293
mugisha-blaine <n.blaine@alustudent.com>
2023-06-06 05:55:37 -0700

    task 7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -2,8 +2,9 @@
 const first = parseInt(process.argv[2]);
 const fun = 'C is fun';
 if (!isNaN(first)){
-	for ( let i = 0; i < first; i++);
-	console.log(fun);
+	for ( let i = 0; i < first; i++){
+	        console.log(fun);
+	}
 
 } else {
 	console.log('Missing number of occurrences');
