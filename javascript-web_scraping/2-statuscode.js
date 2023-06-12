579673f27816dd555b2e465ce8bde19d69a79214
mugisha-blaine <n.blaine@alustudent.com>
2023-06-12 05:15:14 -0700

    task 3&4

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 
 const url = process.argv[2];
 const request = require('request');
-request(url[2], function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else console.log('code:', response && response.statusCode);
--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 // A script that prints the number of movies where the character Wedge Antilles is present
 
-let url = process.argv[2];
-let request = require('request');
+const url = process.argv[2];
+const request = require('request');
 request(url, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
-    let json= JSON.parse(body);
-    let results = json['results'];
+    const json= JSON.parse(body);
+    const results = json['results'];
     let count = 0;
-    for (let i = 0; i < results.length; i++) {
-      let chars = (results[i]['characters']);
+    const (let i = 0; i < results.length; i++) {
+      const chars = (results[i]['characters']);
       for (let j = 0; j < chars.length; j++) {
-        let check = chars[j].endsWith('18/');
+        const check = chars[j].endsWith('18/');
         if (check) {
           count++;
         }
